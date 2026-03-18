dc355755968048133296fadd437a403e9ad49e47
shruti chavda <chavdashruti50@gmail.com>
2026-03-18 15:01:44 +0530

    Add files via upload

---
+++ loops_task.py
@@ -0,0 +1,66 @@
+# Task 1 : print numbers from 1 to 10 using for loop
+# for i in range(1, 11):
+#     print(i)
+
+# Task 2 : print even num from 1 to 20
+# for i in range(2, 21,2):
+#         print(i, end=" ")
+
+# second method :
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i, end=" ")
+
+# Task 3 : print odd num from 1 to 15
+#start: 1
+#end:16
+#step:2
+# for i in range(1, 16,2):
+#         print(i, end=" ")
+
+#for desending order
+
+# for i in range(15, 0, -2):
+#     print(i, end=" ")
+
+# Task 4 : Print each character of the string
+# text = "Python"
+# for i in text:
+#     print(i)
+
+# TAsk 5 : print all items in list
+# items = ["Data","Science","AI"]
+# for i in items:
+#     print(i)
+
+#Task 6 : Find the sum of numbers from 1 to 10
+# sum = 0
+# for i in range(1, 11):
+#     sum += i
+# print(sum)
+
+# Task  7 : print the multiplication table of 5
+
+# for i in range(1, 11): # specific 5 table che etle nai to num variable banavi ne delacare kri skay
+#     print(f"5 x {i} = {5*i}")
+# f = format string, allow embedding expressions inside string literals, using curly braces {}.
+
+# Task 8 : count how many vowels are in string
+# text = "Hello world"
+# count = 0
+# for i in text:
+#     if i in "aeiouAEIOU":
+#         count += 1
+# print(f"Numbers of vowels : {count}")
+
+# Task 9 : print numbers in reverse order from 10 to 1
+# for i in range(10, 0, -1):
+#     print(i, end=" ")
+
+# Task 10 : print square of numbers from 1 to 5
+# for i in range(1, 6):
+#     print(f"Square of {i} is {i**2}")
+
+
+
+
